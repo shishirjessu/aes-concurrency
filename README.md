df94a9381aca3cc0a9b5001cec95d4f00c37fbed
Aneesh <aneesh.adhikari@gmail.com>
2018-05-03 02:17:45 -0500

    updated script, readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # aes-concurrency
-Parallelizations of AES for CS378. A better readme will be up soon, see the pdf if you want more details
+Parallelizations of AES for CS378: Concurrency (Prof. Chris Rossbach, UT Austin). A better readme will be up soon, see the pdf if you want more details
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from subprocess import check_output
 
-rules = ['run_c_', 'run_go_']
+rules = ['make run_c_', 'make run_go_']
 types = ['seq_', 'par_']
 sizes = ['sm', 'md', 'lg', 'xlg', 'xxlg', 'xxxlg']
 
@@ -15,58 +15,54 @@ par_times_go = []
 
 for i in range(len(sizes)):
     size = sizes[i]
+    print(size, i)
     temp = []
     for _ in range(10):
-        temp.append(float(check_output((rules[0] + types[0] + size), shell=True).decode()))
+        temp.append(float(check_output((rules[0] + types[0] + size), shell=True).decode().split('\n')[1]))
     seq_times_c.append(sum(temp)/10)
 
     temp = []
     for _ in range(10):
-        temp.append(float(check_output((rules[0] + types[1] + size), shell=True).decode()))
+        temp.append(float(check_output((rules[0] + types[1] + size), shell=True).decode().split('\n')[1]))
     par_times_c.append(sum(temp)/10)
 
     temp = []
     for _ in range(10):
-        temp.append(float(check_output((rules[1] + types[0] + size), shell=True).decode()))
+        temp.append(float(check_output((rules[1] + types[0] + size), shell=True).decode().split('\n')[1]))
     seq_times_go.append(sum(temp)/10)
 
     temp = []
     for _ in range(10):
-        temp.append(float(check_output((rules[1] + types[1] + size), shell=True).decode()))
+        temp.append(float(check_output((rules[1] + types[1] + size), shell=True).decode().split('\n')[1]))
     par_times_go.append(sum(temp)/10)
 
 
-c_baseline = seq_times_c[0]
-go_baseline = seq_times_go[0]
 
-seq_speedup_c = [c_baseline/thing for thing in seq_times_c]
-seq_speedup_go = [go_baseline/thing for thing in seq_times_go]
-
-par_speedup_c = [c_baseline/thing for thing in par_times_c]
-par_speedup_go = [go_baseline/thing for thing in par_times_go]
+par_speedup_c = [seq_times_c[i]/par_times_c[i] for i in range(len(par_times_c))]
+par_speedup_go = [seq_times_go[i]/par_times_go[i] for i in range(len(par_times_go))]
 
 f = open('output.txt', 'w+')
 
 f.write('seq times c\n')
 f.write(str(seq_times_c))
+f.write('\n')
 
 f.write('seq times go\n')
 f.write(str(seq_times_go))
+f.write('\n')
 
 f.write('par times c\n')
 f.write(str(par_times_c))
+f.write('\n')
 
 f.write('par times go\n')
 f.write(str(par_times_go))
-
-f.write('seq speedup c\n')
-f.write(str(seq_speedup_c))
-
-f.write('seq speedup go\n')
-f.write(str(seq_speedup_go))
+f.write('\n')
 
 f.write('par speedup c\n')
 f.write(str(par_speedup_c))
+f.write('\n')
 
 f.write('par speedup go\n')
 f.write(str(par_speedup_go))
+f.write('\n')
